6a9b8c600d1a34328c68b513d402a854968e0b62
AngeloMastandrea04 <a.mastandrea@studenti.unisa.it>
2025-12-15 15:58:27 +0100

    Update DashboardGeneraleControllerTest.java e Update delle View delle tre sezioni
    Test:
    -Implementazione in DashboardGeneraleController.java dei metodi che testano i pulsanti di "Torna indietro" alla DashboardGenerale presenti nelle tre Sezioni;
    -Aggiunta dell'id per i bottoni di "Torna indietro" nelle View delle tre Sezioni.

--- BibliotecaUniversitaria_G19/src/test/java/G19/Biblioteca/DashboardGeneraleControllerTest.java
+++ BibliotecaUniversitaria_G19/src/test/java/G19/Biblioteca/DashboardGeneraleControllerTest.java
@@ -35,8 +35,9 @@ public class DashboardGeneraleControllerTest extends ApplicationTest {
     void testApriSezUtenti() {
         System.out.print("test Sezione Utenti: ");
         clickOn("#btnSezUtenti");
-        WaitForAsyncUtils.waitForFxEvents();
+        WaitForAsyncUtils.waitForFxEvents();    // Aspetta che venga caricata la scena per sincronizzare i controlli
 
+        // Restituisce un valore non null (il nodo trovato) solo se trova un nodo contenente il testo specificato come parametro
         assertNotNull(lookup("Sezione Utenti").query());
         System.out.println("OK");
     }
@@ -68,6 +69,66 @@ public class DashboardGeneraleControllerTest extends ApplicationTest {
         WaitForAsyncUtils.waitForFxEvents();
 
         assertNotNull(lookup("Sezione Prestiti").query());
+        System.out.println("OK");
+    }
+    
+    /**
+     * Test of tornaIndietro method, of class SezioneUtentiController.
+     * Dopo aver cliccato sul bottone per spostarsi nella Sezione Utenti,
+     * clicca sul bottone per tornare indietro e verifica che sia presente 
+     * una label con su scritto "Biblioteca Universiataria".
+     */
+    @Test
+    void testTornaIndietroSezUtenti() {
+        System.out.print("test Sezione Utenti con TornaIndietro: ");
+        clickOn("#btnSezUtenti");
+        WaitForAsyncUtils.waitForFxEvents();
+        
+        clickOn("#btnIndietroDaSezUtenti");
+        WaitForAsyncUtils.waitForFxEvents();
+        
+        assertNotNull(lookup("Biblioteca Universitaria").query());
+        
+        System.out.println("OK");
+    }
+    
+    /**
+     * Test of tornaIndietro method, of class SezioneLibriController.
+     * Dopo aver cliccato sul bottone per spostarsi nella Sezione Libri,
+     * clicca sul bottone per tornare indietro e verifica che sia presente 
+     * una label con su scritto "Biblioteca Universiataria".
+     */
+    @Test
+    void testTornaIndietroSezLibri() {
+        System.out.print("test Sezione Libri con TornaIndietro: ");
+        clickOn("#btnSezLibri");
+        WaitForAsyncUtils.waitForFxEvents();
+        
+        clickOn("#btnIndietroDaSezLibri");
+        WaitForAsyncUtils.waitForFxEvents();
+        
+        assertNotNull(lookup("Biblioteca Universitaria").query());
+        
+        System.out.println("OK");
+    }
+    
+    /**
+     * Test of tornaIndietro method, of class SezionePrestitiController.
+     * Dopo aver cliccato sul bottone per spostarsi nella Sezione Prestiti,
+     * clicca sul bottone per tornare indietro e verifica che sia presente 
+     * una label con su scritto "Biblioteca Universiataria".
+     */
+    @Test
+    void testTornaIndietroSezPrestiti() {
+        System.out.print("test Sezione Prestiti con TornaIndietro: ");
+        clickOn("#btnSezPrestiti");
+        WaitForAsyncUtils.waitForFxEvents();
+        
+        clickOn("#btnIndietroDaSezPrestiti");
+        WaitForAsyncUtils.waitForFxEvents();
+        
+        assertNotNull(lookup("Biblioteca Universitaria").query());
+        
         System.out.println("OK");
     }
 }
